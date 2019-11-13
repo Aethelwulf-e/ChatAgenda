3d29de08d2eb1bc8e4595eb85a6881eef5687f5f
AlessSilva <alessandrosouza.eq@gmail.com>
2019-11-13 14:04:55 -0300

    firebase configurado

--- app/src/main/java/com/ufc/br/chatagenda/Firebase/DataBaseFirebase.java
+++ app/src/main/java/com/ufc/br/chatagenda/Firebase/DataBaseFirebase.java
@@ -3,23 +3,23 @@ package com.ufc.br.chatagenda.Firebase;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class DataBase {
+public class DataBaseFirebase implements Database {
 
-    private static DataBase dataBase = null;
+    private static DataBaseFirebase dataBase = null;
     private static FirebaseDatabase firebaseDatabase = null;
     private static DatabaseReference databaseReference = null;
 
-    private DataBase(){ }
+    private DataBaseFirebase(){ }
 
     private static void initFirebase(){
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference();
     }
 
-    public static DataBase getInstance(){
+    public static DataBaseFirebase getInstance(){
 
         if ( dataBase == null ){
-            dataBase = new DataBase();
+            dataBase = new DataBaseFirebase();
         }
 
         initFirebase();
@@ -27,6 +27,5 @@ public class DataBase {
         return dataBase;
 
     }
-
 
 }
---
+++ app/src/main/java/com/ufc/br/chatagenda/Firebase/Database.java
@@ -0,0 +1,7 @@
+package com.ufc.br.chatagenda.Firebase;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public interface Database {
+}
