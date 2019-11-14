378a8ca6898b9e95afc8fdca0ffd64c6bd160267
Carlos <carlosferreirahd@gmail.com>
2019-11-14 11:24:57 -0300

    Implementando Frond-End

---
+++ app/src/main/java/com/ufc/br/chatagenda/CustomAdapters/CustomListAdapter.java
@@ -0,0 +1,63 @@
+package com.ufc.br.chatagenda.CustomAdapters;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+
+import com.ufc.br.chatagenda.Model.User;
+import com.ufc.br.chatagenda.R;
+
+import java.util.List;
+
+public class CustomListAdapter extends BaseAdapter {
+    private Activity context;
+    private List<User> users;
+
+    public CustomListAdapter(Activity context, List<User> users) {
+        this.context = context;
+        this.users = users;
+    }
+
+    @Override
+    public int getCount() {
+        return users.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return users.get(position);
+    }
+
+    @Override
+    public View getView(int position, View contextView, ViewGroup parent) {
+        LayoutInflater li = context.getLayoutInflater();
+        View rowView = li.inflate(R.layout.custom_list_layout, null, true);
+
+        TextView tv1 = rowView.findViewById(R.id.item1);
+        TextView tv2 = rowView.findViewById(R.id.item2);
+
+        String nome = users.get(position).getNome();
+        int numero = users.get(position).getNumero();
+
+        tv1.setText(nome);
+        tv1.setTextSize(15);
+
+        tv2.setText(String.valueOf(numero));
+        tv2.setTextSize(12);
+
+        return rowView;
+    }
+
+    public void remove(int position) {
+        users.remove(position);
+    }
+}
--- app/src/main/java/com/ufc/br/chatagenda/Model/User.java
+++ app/src/main/java/com/ufc/br/chatagenda/Model/User.java
@@ -1,4 +1,68 @@
 package com.ufc.br.chatagenda.Model;
 
-public class User {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class User implements Serializable {
+    private int id;
+    private int numero;
+    private String nome;
+    private String senha;
+    private List<User> amigos;
+
+    public User(int id, String nome, int numero, String senha) {
+        this.nome = nome;
+        this.id = id;
+        this.numero = numero;
+        this.senha = senha;
+        this.amigos = new ArrayList<User>();
+    }
+
+    public User(String nome, int numero, String senha) {
+        this.nome = nome;
+        this.numero = numero;
+        this.senha = senha;
+        this.amigos = new ArrayList<User>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void addAmigo(User amigo) {
+
+    }
+
+    public void deleteAmigo(int id) {
+
+    }
 }
